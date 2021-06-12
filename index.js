511584cf1f8232ecfcdfad2c29266b650441d9d1
Johan Wallinder <johan.wallinder@funnel.io>
2021-06-13 01:40:59 +0200

    calculating min and max windshift from average twd values

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ module.exports = function (app) {
 
   var unsubscribes = [];
 
+  var buffer = [];
+  var data = [];
+
+  buffer_timeout_s = 10; //seconds
+  timeseries_timeout_s = 60;
+
   plugin.start = function (options, restartPlugin) {
     // Here we put our plugin logic
     app.debug("Plugin Windshift started");
@@ -31,10 +37,9 @@ module.exports = function (app) {
       (delta) => {
         values = [];
         delta.updates.forEach((u) => {
-          app.debug(u);
-          vals = onWindDirectionTrue(u.values[0].value);
-          app.debug("vals: " + vals);
-          console.log(vals);
+          //app.debug(u);
+          vals = onWindDirectionTrue(u.timestamp, u.values[0].value);
+          //app.debug("vals: " + vals);
           values = values.concat(vals);
         });
 
@@ -55,10 +60,81 @@ module.exports = function (app) {
     );
   };
 
-  function onWindDirectionTrue(twd_rad) {
+  var max = 0;
+  var min = 0;
+
+  const maxreducer = (max, currentValue) =>
+    currentValue > max ? currentValue : max;
+  const minreducer = (min, currentValue) =>
+    currentValue < min ? currentValue : min;
+  const timeMapper = (datapoint) => datapoint[0];
+  const twdMapper = (datapoint) => datapoint[1];
+
+  function onWindDirectionTrue(time, twd_rad) {
+    buffer.push([time, parseFloat(twd_rad)]);
+
+    console.log(buffer);
+
+    if (Date.now() - Date.parse(buffer[0][0]) > buffer_timeout_s * 1000) {
+      // https://math.stackexchange.com/a/1920805
+      //u_east = mean(sin(WD * pi/180))
+      //u_north = mean(cos((WD * pi) / 180));
+      //unit_WD = (arctan2(u_east, u_north) * 180) / pi; (-180 < unit_WD < 180)
+      //unit_WD = (360 + unit_WD) % 360; (0 < unit_WD < 360)
+
+      u_east =
+        buffer
+          .map(twdMapper)
+          .map((twd) => Math.sin(twd))
+          .reduce((acc, u) => acc + u) / buffer.length;
+      u_north =
+        buffer
+          .map(twdMapper)
+          .map((twd) => Math.cos(twd))
+          .reduce((acc, u) => acc + u) / buffer.length;
+      avg_twd = Math.atan2(u_east, u_north);
+
+      avg_twd = (2 * Math.PI + avg_twd) % (2 * Math.PI);
+      app.debug(`avg_twd: ${avg_twd} => ${(avg_twd * 180) / Math.PI}`);
+
+      buffer = [];
+
+      data.push([time, avg_twd]);
+
+      offset = data[0][1];
+      //=if(C3>180,C3-360,if(C3<-180,mod(C3+360,360),C3))
+      diff_array = data
+        .map(twdMapper)
+        .map((twd) => {
+          diff = twd - offset;
+          return diff;
+        })
+        .map((diff) => {
+          if (diff > Math.PI) {
+            return diff - 2 * Math.PI;
+          } else if (diff < -Math.PI) {
+            return (diff + 2 * Math.PI) % (2 * Math.PI);
+          }
+          return diff;
+        });
+
+      min = offset + Math.min(...diff_array);
+      max = offset + Math.max(...diff_array);
+      app.debug(
+        `min: ${min}, max: ${max} from offset: ${offset} and diff_arry: ${diff_array}`
+      );
+
+      app.debug(data);
+      data = data.filter((datapoint) => {
+        time = datapoint[0];
+        filter = Date.now() - Date.parse(time) < timeseries_timeout_s * 1000;
+        return filter;
+      });
+    }
+
     app.debug("twd: " + twd_rad);
-    max = parseFloat(twd_rad) + 0.5;
-    min = parseFloat(twd_rad) - 0.5;
+    //max = parseFloat(twd_rad) + 0.5;
+    //min = parseFloat(twd_rad) - 0.5;
     msg = [
       { path: "environment.wind.windshift.max", value: max },
       { path: "environment.wind.windshift.min", value: min },
